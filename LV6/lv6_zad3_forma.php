57c9d44d6019d2c1a82ccf9c12dffc48313aadc1
iTomsic <patriktomsic2@gmail.com>
2025-11-29 18:12:07 +0100

    Add lv6_zad3_forma.php

---
+++ LV6/lv6_zad3_forma.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forma</title>
+</head>
+<body>
+    
+    <form action="lv6_zad3_prijava.php" method="GET">
+        <label>E-mail: </label><br>
+            <input type="text" name="email"><br>
+        <label>Password </label><br>
+            <input type="password" name="password"><br>
+        
+        <input type="submit" value="Prijava">
+    </form>
+    <br>
+    <br>
+
+</body>
+</html>
